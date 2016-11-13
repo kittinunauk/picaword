45302ac6cbfc12a7f906fa1240ec2a5244154a21
picaword <kittinun.auk@gmail.com>
2016-11-13 16:55:55 +0700

    5th Update

--- php/cards_retrieve.php
+++ php/cards_retrieve.php
@@ -1,27 +1,6 @@
 <?php
-//mysqli() method
-// header("Access-Control-Allow-Origin: *");
-// header("Content-Type: application/json; charset=UTF-8");
 
-// $deckid = $_GET["deckid"];
-// $conn = new mysqli("localhost", "root", "", "picaword");
-// $result = $conn->query("SELECT * FROM Card WHERE CDID=".$deckid."");
-
-// $outp = "";
-// while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-//     if ($outp != "") {$outp .= ",";}
-//     $outp .= '{"CID":"'  . $rs["CID"] . '",';
-//     $outp .= '"CIPath":"'   . $rs["CIPath"] . '",';
-//     $outp .= '"CDescription":"'   . $rs["CDescription"]        . '",';
-//     $outp .= '"CWord":"'   . $rs["CWord"]   . '"}';
-// }
-// $outp ='{"records":['.$outp.']}';
-
-// $conn->close();
-// //Return as JSON object.
-// echo($outp);
-
-//PDO method
+/*PDO*/
 	require("config.php");
 	$deckid = $_GET["deckid"];
 	$pdo = $dbConnection;
--- php/deck_retrieve.php
+++ php/deck_retrieve.php
@@ -1,31 +1,4 @@
 <?php
-/* mysqli() */
-// header("Access-Control-Allow-Origin: *");
-// header("Content-Type: application/json; charset=UTF-8");
-
-// $conn = new mysqli("localhost", "root", "", "picaword");
-// //$result = $conn->query("SELECT * FROM Deck ");
-// $result = $conn->query("SELECT DID,DName,DDescription,DMax,DCreator,DRating,CIPath FROM Deck,Card WHERE Card.CDID = Deck.DID GROUP BY Card.CDID");
-
-// $outp = "";
-
-// while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-//     if ($outp != "") {$outp .= ",";}
-//     $outp .= '{"DID":"'  . $rs["DID"] . '",';
-//     $outp .= '"DName":"'   . $rs["DName"] . '",';
-//     $outp .= '"DDescription":"'   . $rs["DDescription"] . '",';
-//     $outp .= '"DMax":"'   . $rs["DMax"]        . '",';
-//     $outp .= '"DCreator":"'   . $rs["DCreator"]        . '",';
-//     $outp .= '"DRating":"'   . $rs["DRating"]        . '",';
-//     $outp .= '"CIPath":"'   . $rs["CIPath"]   . '"}';
-// }
-
-
-// $outp ='{"records":['.$outp.']}';
-
-// $conn->close();
-// //Return as JSON object.
-// echo($outp);
 
 /*PDO */
 	require("config.php");
