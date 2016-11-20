a4aefdc6c60d78c55c184b88311530b6d34c33e2
midasang <suningmida@hotmail.com>
2016-11-20 12:18:46 +0700

    add deck

--- adddeck.php
+++ adddeck.php
@@ -9,6 +9,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="UTF-8">
   <title>Add deck</title>
   <!-- Include AngularJS Framework &modules -->
   <script type="text/javascript" src="node_modules/angular/angular.min.js"></script>
@@ -25,46 +26,82 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
   <link rel="stylesheet" href="css/style.css">
+
 </head>
-
 <body ng-app="app" ng-controller="addDeckCtrl">
+    <div id="wrapper">
+        <div class="overlay"></div>
+        <!-- Sidebar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+            <ul class="nav sidebar-nav">
+                <li class="sidebar-brand">
+                   <img id="nohover" src="img/web/LOGOMINI.png" alt="" align="center">
+                </li>
+                <li>
+                    <a href="main.php">
+                    <i class="fa fa-user"></i><?php echo " User: <b>".$_SESSION['UUser']."</b>!";?>
+                    </a>
+                </li>
+                <li>
+                    <a href="main.php">
+                    <i class="fa fa-fw fa-home"></i>  Home <span class="badge">2</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="php/logout.php">
+                    <i class="fa fa-key"></i> Logout
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /#sidebar-wrapper -->
 
-    <h2>My Created Deck</h2>
+<!-- Page Content -->
+        <div id="page-content-wrapper">
+          <button type="button" class="hamburger is-closed animated fadeInLeft" data-toggle="offcanvas">
+            <span class="hamb-top"></span>
+            <span class="hamb-middle"></span>
+            <span class="hamb-bottom"></span>
+          </button>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                       <h2>My Created Deck</h2>
     <div ng-repeat="n in mydeck" style="display: inline-block;">
 
-	<img src={{n.DCover}} width="150px" height="200px" data-toggle="modal" data-target ="#{{n.DID}}">
-    	<!-- <button type="button"  data-toggle="modal" data-target="#addcard">Add New card</button> -->
+  <img src={{n.DCover}} width="150px" height="200px" data-toggle="modal" data-target ="#{{n.DID}}">
+      <!-- <button type="button"  data-toggle="modal" data-target="#addcard">Add New card</button> -->
 
-	<!-- Modal for display information-->
-	<div id="{{n.DID}}" class="modal fade" role="dialog">
-	      <div class="modal-dialog">
+  <!-- Modal for display information-->
+  <div id="{{n.DID}}" class="modal fade" role="dialog">
+        <div class="modal-dialog">
 
-	        <!-- Modal content-->
-	        <div class="modal-content">
-	          <div class="modal-header">
-	            <button type="button" class="close" data-dismiss="modal">&times;</button>
-	            <h4 class="modal-title">Deck Information</h4>
-	          </div>
-	          <div class="modal-body">
-	            <p><b>Deck Name: </b> {{n.DName}}<br>
-	                   <b> Description: </b>{{n.DDescription}} <br>
-	            </p>
-	          </div>
-	          <div class="modal-footer">
-	          <form action="addcard.php" method="POST">
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Deck Information</h4>
+            </div>
+            <div class="modal-body">
+              <p><b>Deck Name: </b> {{n.DName}}<br>
+                     <b> Description: </b>{{n.DDescription}} <br>
+              </p>
+            </div>
+            <div class="modal-footer">
+            <form action="addcard.php" method="POST">
 
-	            <input type="text" value="{{n.DName}}" name="DName" ng-hide="true"/>
-	            <input type="text" value="{{n.DID}}" name="DID" ng-hide="true"/>
-	            <button type="submit" class="btn btn-warning"> Add Card</button>
-	            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-	           </form>
-	           
-	          </div>
-	         
-	        </div>
+              <input type="text" value="{{n.DName}}" name="DName" ng-hide="true"/>
+              <input type="text" value="{{n.DID}}" name="DID" ng-hide="true"/>
+              <button type="submit" class="btn btn-warning"> Add Card</button>
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+             </form>
+             
+            </div>
+           
+          </div>
 
-	      </div>
-	  </div>
+        </div>
+    </div>
 
 
 
@@ -99,7 +136,19 @@
 
       </div>
     </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
 
+    </div>
+    <!-- /#wrapper -->
+    
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
+
+<script src="js/index.js"></script>
 
 </body>
 <script>
@@ -108,36 +157,36 @@
       
 
          $http.get('php/mydeck_retreive.php').then(function (response) {
-         	$scope.mydeck = response.data.decklist;
-         	console.log($scope.mydeck);
+          $scope.mydeck = response.data.decklist;
+          console.log($scope.mydeck);
          });
 
          $scope.submitForm = function() {
-         	 // create a blank object to handle form data.
-        	         $scope.user = {};
-        	         $scope.user.cname = $scope.cardName;
-        	         $scope.user.cdes = $scope.cardDes;
-        	         $scope.user.ccate = $scope.cardCate;
-	         $scope.user.cimg = $scope.myCroppedImage;
-	        // Posting data to php file
-	        $http({
-	          method  : 'POST',
-	          url     : 'php/cards_insert.php',
-	          data    : $scope.user, //forms user object
-	          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
-	         })
-	          .success(function(data) {
-	            if (data.errors) {
-	              // Showing errors.
-	              $scope.errorName = data.errors.name;
-	              $scope.errorUserName = data.errors.username;
-	              $scope.errorEmail = data.errors.email;
-	              $scope.errorImg = data.errors.cimg;
-	            } else {
-	              $scope.message = data.message;
-	              console.log($scope.message);
-	            }
-	          });
+           // create a blank object to handle form data.
+                   $scope.user = {};
+                   $scope.user.cname = $scope.cardName;
+                   $scope.user.cdes = $scope.cardDes;
+                   $scope.user.ccate = $scope.cardCate;
+           $scope.user.cimg = $scope.myCroppedImage;
+          // Posting data to php file
+          $http({
+            method  : 'POST',
+            url     : 'php/cards_insert.php',
+            data    : $scope.user, //forms user object
+            headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+           })
+            .success(function(data) {
+              if (data.errors) {
+                // Showing errors.
+                $scope.errorName = data.errors.name;
+                $scope.errorUserName = data.errors.username;
+                $scope.errorEmail = data.errors.email;
+                $scope.errorImg = data.errors.cimg;
+              } else {
+                $scope.message = data.message;
+                console.log($scope.message);
+              }
+            });
         };
 
         $scope.myImage='';
